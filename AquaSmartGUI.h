630a27304b75ad91a145418e46162ee8ccef271f
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2017-08-26 15:08:06 +0200

    GUI methods added

--- AquaSmartGUI.h
+++ AquaSmartGUI.h
@@ -9,7 +9,9 @@ class AquaSmartGUI
     AquaSmartGUI();
     void draw_logo();
     void draw_start(boolean& finish);
-    void draw_temperature(boolean fanIsOn, float temp, float last_temp, float delta, int fan, int current_index, int total_elements);
+    void draw_temperature(boolean fanIsOn, float temp, boolean temp_is_growing, int fan, int current_index, int total_elements);
+    void draw_water_level(int level, int current_index, int total_elements);
+    void draw_light(boolean light_is_on, int current_index, int total_elements);
     void draw_menu_item(int item_index, char *item_name); // Menu item preview
   private:
   	int progress;
